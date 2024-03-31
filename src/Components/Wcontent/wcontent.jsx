afb31c9fcb1d742ee078dbd07837ba5efb692c72
Sergio <114320133+sergioing1000@users.noreply.github.com>
2024-03-31 08:14:41 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/Components/Wcontent/Wcontent.css
    	modified:   src/Components/Wcontent/wcontent.jsx
    	new file:   src/assets/icons/burger.png
    	new file:   src/assets/icons/frenchfries.png
    	new file:   src/assets/icons/soda.png
    	renamed:    src/assets/images/menu/Fav2.jpeg -> src/assets/images/menu/fav2.jpg

--- src/Components/Wcontent/wcontent.jsx
+++ src/Components/Wcontent/wcontent.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import "./Wcontent.css";
 
 import Fav1 from "../../assets/images/menu/fav1.jpg";
+import Fav2 from "../../assets/images/menu/fav2.jpg";
+import Burger from "../../assets/icons/burger.png";
+import Ffries from "../../assets/icons/frenchfries.png";
+import Soda from "../../assets/icons/soda.png";
 
 const Wcontent = () => {
 
@@ -12,12 +16,33 @@ const Wcontent = () => {
         <img className="Wcontimg" src={Fav1} alt="Favorit01" />
         <p className="Wconttext">American Burguer</p>
         <div className="Wconticons">
-          texto3
+          <img src={Burger} alt="Burger Icon" width={40} />
+          <span>+</span>
+          <img src={Ffries} alt="Ffries Icon" width={40} />
+          <span>+</span>
+          <img src={Soda} alt="Soda Icon" width={40} />
         </div>
-        <pre>texto4</pre>
+        <div>
+          <span className="WcontPrice1">$12,</span>
+          <span className="WcontPrice2">40</span>
+        </div>
       </div>
 
-      <div className="WcontentR">div2</div>
+      <div className="WcontentR">
+        <img className="Wcontimg" src={Fav2} alt="Favorit02" />
+        <p className="Wconttext">American Burguer</p>
+        <div className="Wconticons">
+          <img src={Burger} alt="Burger Icon" width={40} />
+          <span>+</span>
+          <img src={Ffries} alt="Ffries Icon" width={40} />
+          <span>+</span>
+          <img src={Soda} alt="Soda Icon" width={40} />
+        </div>
+        <div>
+          <span className="WcontPrice1">$14,</span>
+          <span className="WcontPrice2">40</span>
+        </div>
+      </div>
     </body>
   );
 
