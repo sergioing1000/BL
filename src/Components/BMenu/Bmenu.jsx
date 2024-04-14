b0665ac9b1c9bc116b5dfc5cefd362911eb174be
Sergio <114320133+sergioing1000@users.noreply.github.com>
2024-04-13 19:10:53 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   public/images/drinks/001.jpg
    	new file:   public/images/drinks/002.jpg
    	new file:   public/images/drinks/003.jpg
    	new file:   public/images/drinks/004.jpg
    	new file:   public/images/drinks/005.jpg
    	new file:   public/images/drinks/006.jpg
    	new file:   public/images/drinks/007.jpg
    	new file:   public/images/drinks/008.jpg
    	new file:   public/images/drinks/009.jpg
    	new file:   public/images/drinks/010.jpg
    	new file:   public/images/drinks/011.jpg
    	new file:   public/images/drinks/012.jpg
    	new file:   public/images/drinks/013.jpg
    	new file:   public/images/drinks/014.jpg
    	new file:   public/images/drinks/015.jpg
    	new file:   public/images/drinks/016.jpg
    	new file:   public/images/drinks/017.jpg
    	new file:   public/images/drinks/018.jpg
    	new file:   public/images/drinks/019.jpg
    	new file:   public/images/drinks/020.jpg
    	new file:   public/images/drinks/021.jpg
    	new file:   public/images/drinks/022.jpg
    	new file:   public/images/drinks/023.jpg
    	new file:   public/images/drinks/024.jpg
    	new file:   public/images/drinks/025.jpg
    	new file:   public/images/drinks/026.jpg
    	new file:   public/images/drinks/027.jpg
    	new file:   public/images/drinks/028.jpg
    	new file:   public/images/drinks/029.jpg
    	new file:   public/images/drinks/030.jpg
    	new file:   public/images/drinks/031.jpg
    	new file:   public/images/drinks/032.jpg
    	new file:   public/images/drinks/033.jpg
    	new file:   public/images/drinks/034.jpg
    	new file:   public/images/drinks/035.jpg
    	new file:   public/images/drinks/036.jpg
    	new file:   public/images/drinks/037.jpg
    	new file:   public/images/drinks/038.jpg
    	new file:   public/images/drinks/039.jpg
    	new file:   public/images/drinks/040.jpg
    	new file:   public/images/drinks/041.jpg
    	new file:   public/images/drinks/042.jpg
    	new file:   public/images/drinks/043.jpg
    	new file:   public/images/drinks/044.jpg
    	new file:   public/images/drinks/045.jpg
    	new file:   public/images/drinks/046.jpg
    	new file:   public/images/drinks/047.jpg
    	modified:   src/Components/BMenu/Bmenu.jsx

--- src/Components/BMenu/Bmenu.jsx
+++ src/Components/BMenu/Bmenu.jsx
@@ -24,6 +24,14 @@ function BMenu() {
     slidesToScroll: 1,
   };
 
+  const settings3 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div className="Bmenutop">
@@ -73,6 +81,39 @@ function BMenu() {
           <div>
             <Slider {...settings2}>
               {data2.map((d) => (
+                <div className="card">
+                  <div className="topCard">
+                    <div className="cardimageframe">
+                      <img
+                        className="cardimage"
+                        src={d.img}
+                        alt={d.name}
+                        width={140}
+                      />
+                    </div>
+                  </div>
+                  <div className="bottomCard">
+                    <p className="Cardtextname">{d.name}</p>
+                    <span className="CardPrice1">{d.Price1}</span>
+                    <span className="CardPrice2">{d.Price2}</span>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+
+      <div className="BMenu3">
+        <div className="Bmenutop2">
+          <p className="BmenuTop2Title2">Drinks</p>
+          <span className="BmenuTop2Line2"></span>
+        </div>
+
+        <div className="Carousel">
+          <div>
+            <Slider {...settings3}>
+              {data3.map((d) => (
                 <div className="card">
                   <div className="topCard">
                     <div className="cardimageframe">
@@ -220,6 +261,291 @@ const data2 = [
   {
     name: "Serving of eggs",
     img: "/images/fastfood/012.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+];
+
+const data3 = [
+  {
+    name: "dopi",
+    img: "/images/drinks/001.jpg",
+    Price1: "$26,",
+    Price2: "50",
+  },
+  {
+    name: "fenta",
+    img: "/images/drinks/002.jpg",
+    Price1: "$22,",
+    Price2: "25",
+  },
+  {
+    name: "coda",
+    img: "/images/drinks/003.jpg",
+    Price1: "$19,",
+    Price2: "90",
+  },
+  {
+    name: "foble",
+    img: "/images/drinks/004.jpg",
+    Price1: "$20,",
+    Price2: "75",
+  },
+  {
+    name: "topble",
+    img: "/images/drinks/005.jpg",
+    Price1: "$23,",
+    Price2: "00",
+  },
+  {
+    name: "Feva",
+    img: "/images/drinks/006.jpg",
+    Price1: "$17,",
+    Price2: "50",
+  },
+  {
+    name: "siaba",
+    img: "/images/drinks/007.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Bale",
+    img: "/images/drinks/008.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Saica",
+    img: "/images/drinks/009.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Codd",
+    img: "/images/drinks/010.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca cola Space",
+    img: "/images/drinks/011.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola CAD",
+    img: "/images/drinks/012.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola",
+    img: "/images/drinks/013.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola Ice Berry",
+    img: "/images/drinks/014.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Sesta",
+    img: "/images/drinks/015.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola Orange",
+    img: "/images/drinks/016.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Bodie",
+    img: "/images/drinks/017.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Dobb",
+    img: "/images/drinks/018.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Soia",
+    img: "/images/drinks/019.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola dceh",
+    img: "/images/drinks/020.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola cuich",
+    img: "/images/drinks/021.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola febs",
+    img: "/images/drinks/022.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola cerba",
+    img: "/images/drinks/023.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola green",
+    img: "/images/drinks/024.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola apple",
+    img: "/images/drinks/025.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "AEPPHAA",
+    img: "/images/drinks/026.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Dppae",
+    img: "/images/drinks/027.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola",
+    img: "/images/drinks/028.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Color TE",
+    img: "/images/drinks/029.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Color TE",
+    img: "/images/drinks/030.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Color TEE",
+    img: "/images/drinks/031.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola",
+    img: "/images/drinks/032.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Bendi",
+    img: "/images/drinks/033.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Soda",
+    img: "/images/drinks/034.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Siosid SODA",
+    img: "/images/drinks/035.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Poda",
+    img: "/images/drinks/036.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Donta",
+    img: "/images/drinks/037.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Sorda",
+    img: "/images/drinks/038.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Saba",
+    img: "/images/drinks/039.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "aca Sola",
+    img: "/images/drinks/040.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Senid8",
+    img: "/images/drinks/041.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola Coba",
+    img: "/images/drinks/042.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Spichac",
+    img: "/images/drinks/043.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "SEDNA",
+    img: "/images/drinks/044.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coda",
+    img: "/images/drinks/045.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Soda Dexratt",
+    img: "/images/drinks/046.jpg",
+    Price1: "$37,",
+    Price2: "25",
+  },
+  {
+    name: "Coca Cola",
+    img: "/images/drinks/047.jpg",
     Price1: "$37,",
     Price2: "25",
   },
