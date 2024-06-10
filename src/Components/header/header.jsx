ddce3967c89b48545936b9a1491e804206e077c1
Sergio <114320133+sergioing1000@users.noreply.github.com>
2024-06-10 17:09:54 -0500

    modified:   src/Components/Wcontent/Wcontent.css 	modified:   src/Components/header/header.jsx 	modified:   src/Components/header/headerStyles.css

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -5,29 +5,13 @@ import shoppingcart from "../../assets/icons/shopping-cart-blck.svg";
 import bonusticket from "../../assets/images/ticket.png";
 import loginicon from "../../assets/images/login-icon.jpg";
 
+import Header2 from "./Components/header/navbar.jsx";
 import LoginModal from "../LoginModal/modal.jsx";
 
-import Header2 from "./Components/header/navbar.jsx";
 import "./headerStyles.css";
 
 const Header = () => {
 
-  const [dropdownVisible, setDropdownVisible] = useState(false);
-
-  const toggleDropdown = () => {
-    console.log("Dropdown button clicked");
-    setDropdownVisible(!dropdownVisible);
-  };
-
-  const closeDropdown = () => {
-    setDropdownVisible(false);
-  };
-
-    const myFunction = () => {
-      // Your function logic goes here
-      console.log("Dropdown button clicked");
-  };
-  
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const openModal = () => {
@@ -37,6 +21,9 @@ const Header = () => {
   const closeModal = () => {
     setIsModalVisible(false);
   };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <header className="header_container">
@@ -65,8 +52,31 @@ const Header = () => {
 
       <div className="LoginModal">
         <LoginModal isVisible={isModalVisible} onClose={closeModal}>
-          <h2>Modal Title</h2>
+          <h2>LOGIN</h2>
           <p>This is the modal content.</p>
+
+          <form>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+
         </LoginModal>
       </div>
     </header>
